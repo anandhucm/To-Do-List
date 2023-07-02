bb71562440ba6dac8f6a685b4a11afdd4458fe86
Anandhu C M <anandhumohanan97@gmail.com>
2023-07-03 01:14:57 +0530

    deleted items listing started

---
+++ src/App.js
@@ -0,0 +1,83 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+
+  const [toDos, setTodos] = useState([])
+  const [toDo, setTodo] = useState('')
+  const [delItems, setDel] = useState([])
+  return (
+    <div className="app">
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
+      </div>
+      <div className="subHeading">
+        <br />
+        <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
+      </div>
+      <div className="input">
+        <input value={toDo} onChange={(e) => setTodo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
+        <i onClick={() => setTodos([...toDos, { id: Date.now(), text: toDo, status: false }])} className="fas fa-plus"></i>
+      </div>
+
+      {toDos.map((obj) => {
+
+        return (
+          <div className="todos">
+            <div className="todo">
+              <div className="left">
+                <input onChange={(e) => {
+                  console.log(e.target.checked);
+                  console.log(obj)
+                  setTodos(toDos.filter((obj2) => {
+                    if (obj2.id === obj.id) {
+                      obj2.status = e.target.checked;
+                    }
+                    return obj2;
+                  }))
+                }} value={obj.status} type="checkbox" name="" id="" />
+                <p>{obj.text}</p>
+              </div>
+              <div className="right">
+                <i onClick={(e) => {
+                  setTodos(toDos.filter((obj2) => {
+                    if (obj2.id !== obj.id) {
+                      return obj2
+                    }
+                    setDel([...delItems, obj])
+                    return null
+                  }))
+                }}
+                  className="fas fa-times"></i>
+              </div>
+            </div>
+          </div>)
+      })}
+      <div className="row col-md-6">
+      <h4 className='del'>Deleted Items</h4>
+      </div>
+      { delItems.map((obj) => {
+          return (
+            <div className='del'>
+              <p className='delObj'>{obj.text}</p>
+            </div>
+          )
+        })
+      }
+      <div className="green col-md-6">
+        <h4>Completed Items</h4>
+      </div>
+      {toDos.map((obj) => {
+        if (obj.status) {
+          return (
+            <h1>{obj.text}</h1>
+          )
+        }
+        return null;
+      })}
+    </div>
+  );
+}
+
+export default App;
+
